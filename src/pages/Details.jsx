761a3ff617c5be08eefb94f471e922c45f7462a1
moniwero <mmonisiabb@gmail.com>
2025-01-16 22:13:57 +0100

    page Home

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,11 +1,11 @@
 function Details() {
-    return (
-      <div>
-        <h1>Details Page</h1>
-        <p>This is the details page for a specific result.</p>
-      </div>
-    );
-  }
-  
-  export default Details; // Eksport domyślny
+  return (
+    <div>
+      <h1>Details Page</h1>
+      <p>This is the details page for a specific result.</p>
+    </div>
+  );
+}
+
+export default Details; // Eksport domyślny
   
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
-function Home() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the home page!</p>
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "../styles/Home.scss";
+import HomeIlustration from "../assets/Home-img.jpg";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <div className="home-text">
+          <h1 className="home-title">Moja dokumentacja medyczna</h1>
+          <Button
+            variant="contained"
+            className="home-button"
+            onClick={() => navigate("/login")}
+          >
+            Zaloguj
+          </Button>
+        </div>
+        <div className="home-image">
+          <img src={HomeIlustration} alt="Ilustracja Home" />
+        </div>
       </div>
-    );
-  }
-  
-  export default Home; // Eksport domyślny
+    </div>
+  );
+};
+
+export default Home;
