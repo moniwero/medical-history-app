c38efe47a1199d15ec5128eb287c5eaf986d0f6e
moniwero <mmonisiabb@gmail.com>
2025-01-23 22:50:21 +0100

    poprawa logowania

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -7,15 +7,29 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://iqulcbywphjewenalnex.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxdWxjYnl3cGhqZXdlbmFsbmV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3OTYwODgsImV4cCI6MjA1MjM3MjA4OH0.zLSUvcFNCzehoUizP-R9w5X8EFmJlV7cZEnD6X9vr3E"; 
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxdWxjYnl3cGhqZXdlbmFsbmV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3OTYwODgsImV4cCI6MjA1MjM3MjA4OH0.zLSUvcFNCzehoUizP-R9w5X8EFmJlV7cZEnD6X9vr3E";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
 
 export const uploadImage = async (file) => {
-    const { data, error } = await supabase.storage
-      .from("results") // Upewnij się, że nazwa bucketu jest poprawna
-      .upload(`images/${file.name}`, file);
-  
-    if (error) throw error;
-    return data.path; // Zwraca ścieżkę do zapisanego pliku
-  };
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  console.log(session);
+
+  const { data, error } = await supabase.storage
+    .from("results")
+    .upload(`images/${file.name}`, file, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  if (error) throw error;
+  return data.path; // Zwraca ścieżkę do zapisanego pliku
+};
