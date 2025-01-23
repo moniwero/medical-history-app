f3222a3ed74c2a977940fdc4084a24816c5b295a
moniwero <mmonisiabb@gmail.com>
2025-01-23 14:50:51 +0100

    Dashboard page and back button on home page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,47 @@
-function Dashboard() {
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "../styles/Dashboard.scss";
+import HomeIlustration from "../assets/Home-img.jpg";
+
+const Dashboard = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Manage your medical history here.</p>
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <Button
+          variant="text"
+          className="dashboard-back-button"
+          onClick={() => navigate("/")}
+        >
+          Wstecz
+        </Button>
+      </div>
+
+      <div className="dashboard-content">
+        <div className="dashboard-image">
+          <img src={HomeIlustration} alt="Ilustracja Dashboard" />
+        </div>
+
+        <div className="dashboard-buttons">
+          <Button
+            variant="contained"
+            className="dashboard-button"
+            onClick={() => navigate("/categories")}
+          >
+            Wyniki
+          </Button>
+          <Button
+            variant="contained"
+            className="dashboard-button"
+            onClick={() => navigate("/add-result")}
+          >
+            Dodaj wynik
+          </Button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Dashboard; // Eksport domyślny
+export default Dashboard;
