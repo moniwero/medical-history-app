68aebadc82eb04e5cb27f098414d6993465228ff
moniwero <mmonisiabb@gmail.com>
2025-02-01 14:18:36 +0100

    categorySelect i inne poprawki

---
+++ src/components/CategorySelect.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+
+const CategorySelect = ({ value, onChange }) => {
+  return (
+    <FormControl fullWidth className="file-select">
+      <InputLabel>Kategoria</InputLabel>
+      <Select
+        value={value}
+        onChange={onChange}
+        required
+        className="file-select"
+      >
+        <MenuItem value="">Wybierz kategorię</MenuItem>
+        <MenuItem value="usg">USG</MenuItem>
+        <MenuItem value="rtg">RTG</MenuItem>
+        <MenuItem value="blood-tests">Badania krwi</MenuItem>
+        <MenuItem value="tk">TK</MenuItem>
+        <MenuItem value="mri">MRI</MenuItem>
+        <MenuItem value="echo">ECHO SERCA</MenuItem>
+        <MenuItem value="other">INNE</MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+//Dodanie walidacji propsów
+CategorySelect.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default CategorySelect;
