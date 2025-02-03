c5730e6753e8e8e91948f813aca655773002b6f4
moniwero <mmonisiabb@gmail.com>
2025-02-03 22:50:00 +0100

    dodanie konfiguracji

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", 
   build: {
     outDir: "dist"
   },
   server: {
-    historyApiFallback: true // Obsługuje poprawnie trasy w React Router
+    historyApiFallback: true 
   }
 })
